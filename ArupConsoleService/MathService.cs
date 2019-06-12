803a212d994ef8d7e68a05ac8a334604ab4a4ee1
Prakash Reddy <preddy@newrelic.com>
2019-06-12 17:34:00 +0000

    initial

---
+++ ArupConsoleService/MathService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ArupConsoleService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MathService" in both code and config file together.
+    public class MathService : IMathService
+    {
+
+        int IMathService.Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
